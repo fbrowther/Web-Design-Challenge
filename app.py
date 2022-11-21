458410788388c0a6a838e9dbc590caf2946f588f
Farjana Rowther <fbrowther@gmail.com>
2022-11-21 10:31:54 +0000

    file paths checked

--- app.py
+++ app.py
@@ -15,12 +15,12 @@ df.to_csv('./Resources/cities.csv', index=None)
   
 # route to html page - "table"
 @app.route('/')
-@app.route('/table')
+@app.route('/tables')
 def table():
     
     # converting csv to html
     data = pd.read_csv('./Resources/cities.csv')
-    return render_template('table.html', tables=[data.to_html()], titles=[''])
+    return render_template('tables.html', tables=[data.to_html()], titles=[''])
 
 
 if __name__ == "__main__":
